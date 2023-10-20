40c33faa022296fbe37697b15fc32cfb41bdf7a6
Chu Văn Giá <vangiachu@gmail.com>
2023-10-20 13:24:27 +0700

    finish commit

--- src/app/card-form/card-form.component.ts
+++ src/app/card-form/card-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { DateFormControl } from '../date-form-control';
 
 @Component({
   selector: 'app-card-form',
@@ -8,10 +9,37 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class CardFormComponent implements OnInit {
   cardForm = new FormGroup({
-    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    name: new FormControl('asdf', [
+      Validators.required,
+      Validators.minLength(3),
+    ]),
+    cardNumber: new FormControl('', [
+      Validators.required,
+      Validators.minLength(16),
+      Validators.maxLength(16),
+    ]),
+    expiration: new DateFormControl('', [
+      Validators.required,
+      Validators.pattern(/^(0[1-9]|1[0-2])\/\d{2}$/),
+    ]),
+    securityCode: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(3),
+    ]),
   });
 
-  constructor() {}
+  constructor() {
+    console.log(this.cardForm.controls.name);
+  }
 
   ngOnInit(): void {}
+
+  onSubmit() {
+    console.log('Form was submitted');
+  }
+
+  onResetClick() {
+    this.cardForm.reset();
+  }
 }
